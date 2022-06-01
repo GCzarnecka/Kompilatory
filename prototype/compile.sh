7e4b27637d76b71d20694bbba39f23e10f2b9b84
Laronk <karpiuk.janek@gmail.com>
2022-06-01 16:51:13 +0200

    Fix command parameters order

--- prototype/compile.sh
+++ prototype/compile.sh
@@ -1,5 +1,5 @@
 bison -o parser.cpp -d parser.y
-flex scanner.l -o scanner.cpp
+flex -o scanner.cpp scanner.l
 g++ -c -o scanner.o scanner.cpp
 g++ -c -o parser.o parser.cpp
 g++ main.cpp parser.o scanner.o
