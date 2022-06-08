943fa88c71d4708f36b1d904eea82ba9741d5886
Laronk <karpiuk.janek@gmail.com>
2022-06-08 16:47:48 +0200

    Add generating AST image & update .gitignore

--- prototype/clean.sh
+++ prototype/clean.sh
@@ -1,1 +1,1 @@
-rm -f parser.o parser.cpp parser.hpp stack.hh scanner.cpp scanner.o a.out
+rm -f parser.o parser.cpp parser.hpp stack.hh scanner.cpp scanner.o a.out parser.dot parser.png parser.tab.cc
--- prototype/compile.sh
+++ prototype/compile.sh
@@ -3,3 +3,6 @@ flex -o scanner.cpp scanner.l
 g++ -c -o scanner.o scanner.cpp
 g++ -c -o parser.o parser.cpp
 g++ main.cpp parser.o scanner.o
+# create an .png image of the AST
+bison parser.y --graph
+dot parser.dot -T png -o parser.png
