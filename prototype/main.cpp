6f8c59bc25b05b033e7a78c9de20369e3f33d0e5
Laronk <karpiuk.janek@gmail.com>
2022-06-29 01:06:05 +0200

    Reformat cpp file

--- prototype/main.cpp
+++ prototype/main.cpp
@@ -1,13 +1,15 @@
 #include "ast.hpp"
 #include "parser.hpp"
 
-void yy::parser::error(const std::string& msg) {
+void yy::parser::error(const std::string &msg)
+{
     std::cout << "An error occured: " << msg << std::endl;
 }
 
 extern std::vector<definition_ptr> program;
 
-int main() {
+int main()
+{
     yy::parser parser;
     parser.parse();
     std::cout << "Program size: " << program.size() << std::endl;
